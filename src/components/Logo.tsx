816da87ea3fa37300a42f6d7b46dc36d796cfa73
Daniel Molina <danielfmolina@comfama.com.co>
2024-05-07 23:27:57 -0500

    feat: change logo and search form styles

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/">
-      <img src={import.meta.env.VITE_LOGO_URL} alt="Logo MeLi" />
+      <img src={import.meta.env.VITE_LOGO_URL} height={40} alt="Logo MeLi" />
     </Link>
   );
 };
