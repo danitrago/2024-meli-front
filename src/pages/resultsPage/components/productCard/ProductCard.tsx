846bb844761576742db711db4424fddf5a4ef5c3
Daniel Molina <danielfmolina@comfama.com.co>
2024-05-08 19:22:08 -0500

    feat: add line truncate mixin

--- src/pages/resultsPage/components/productCard/ProductCard.tsx
+++ src/pages/resultsPage/components/productCard/ProductCard.tsx
@@ -3,15 +3,24 @@ import { Link } from "react-router-dom";
 import "./productCard.style.scss";
 
 const ProductCard = (props: Product) => {
-  const { id, title, img, prettyPrice } = props;
+  const { id, title, img, prettyPrice, condition, freeShipping } = props;
   return (
     <article className="productCard">
-      <img className="productCard__img" src={img} alt={title} />
+      <Link className="productCard__imgLink" to={`/items/${id}`}>
+        <img className="productCard__img" src={img} alt={title} />
+      </Link>
       <div className="productCard__desc">
         <span className="productCard__price">{prettyPrice}</span>
-        <Link to={`/items/${id}`}>{title}</Link>
+        {freeShipping && (
+          <span className="productCard__freeShipping">Envío gratis</span>
+        )}
+        <h3 className="productCard__title">
+          <Link to={`/items/${id}`}>{title}</Link>
+        </h3>
       </div>
-      <div className="productCard__flags">Nuevo</div>
+      <div className="productCard__flags">
+        <span>{condition}</span>
+      </div>
     </article>
   );
 };
