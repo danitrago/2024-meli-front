ee3abd93c5cb07073bbd52247dd9eb50b864268d
Daniel Molina <danielfmolina@comfama.com.co>
2024-05-08 21:49:16 -0500

    feat: add ProductSheet styles

--- src/pages/productPage/components/productSheet/ProductSheet.tsx
+++ src/pages/productPage/components/productSheet/ProductSheet.tsx
@@ -1,29 +1,41 @@
 import { Product } from "@/types/product.types";
 import "./productSheet.style.scss";
+import { FreeShipingText } from "@/components/freeShippingText";
+import { BuyButton } from "@/components/buyButton";
 
 const ProductSheet = (props: Product) => {
-  const { id, title, img, prettyPrice, condition, freeShipping, description } =
-    props;
+  const {
+    id,
+    title,
+    img,
+    prettyPrice,
+    condition,
+    freeShipping,
+    description,
+    soldQuantity
+  } = props;
   return (
     <article className="productSheet">
-      <img className="productSheet__img" src={img} alt={title} />
       <div className="productSheet__desc">
+        <div className="productSheet__gallery">
+          <img className="productSheet__thumbImg" src={img} alt={title} />
+          <img className="productSheet__img" src={img} alt={title} />
+        </div>
+        <h2>Descripción del producto</h2>
+        <p>{description}</p>
+      </div>
+      <div className="productSheet__buy">
+        <div className="productSheet__flags">
+          <span>
+            {condition}
+            {soldQuantity && ` - ${soldQuantity} vendidos`}
+          </span>
+        </div>
+        <h1 className="productSheet__title">{title}</h1>
         <span className="productSheet__price">{prettyPrice}</span>
-        {freeShipping && (
-          <span className="productSheet__freeShipping">Envío gratis</span>
-        )}
-        <h3 className="productSheet__title">{title}</h3>
+        {freeShipping && <FreeShipingText />}
+        <BuyButton onClick={() => alert("Comprar " + id)}>Comprar</BuyButton>
       </div>
-      <div className="productSheet__flags">
-        <span>{condition}</span>
-      </div>
-      <p>{description}</p>
-      <button
-        className="productSheet__button"
-        onClick={() => alert("Comprar" + id)}
-      >
-        Comprar
-      </button>
     </article>
   );
 };
