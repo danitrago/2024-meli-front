126b77b4b0c8d1f45323989888fcacbb77db33c3
Daniel Molina <danielfmolina@comfama.com.co>
2024-05-08 20:53:34 -0500

    feat: setip product page

--- src/pages/productPage/ProductPage.tsx
+++ src/pages/productPage/ProductPage.tsx
@@ -1,30 +1,12 @@
 import { Layout } from "@/layout";
 import Container from "@/layout/Container";
-import { getProductDetails } from "@/services/products.service";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import ProductDetails from "./containers/ProductDetails";
 
 const ProductPage = () => {
-  let { productId } = useParams();
-  const [product, setProduct] = useState<any>({});
-
-  const fetchData = async () => {
-    if (!productId) return;
-    const data = await getProductDetails(productId);
-    console.log(data.data.item);
-    setProduct(data.data.item);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <Layout>
       <Container>
-        ProductDetails of: {JSON.stringify(productId)}
-        <hr />
-        {product.title}
+        <ProductDetails />
       </Container>
     </Layout>
   );
