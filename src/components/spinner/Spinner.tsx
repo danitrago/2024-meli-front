eefeb2771f5adaa417e59fb9cf5c19856bf5355d
Daniel Molina <danielfmolina@comfama.com.co>
2024-05-08 20:12:44 -0500

    feat: add Spinner component

---
+++ src/components/spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import "./spinner.style.scss";
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner__loader"></div>
+    </div>
+  );
+};
+
+export default Spinner;
